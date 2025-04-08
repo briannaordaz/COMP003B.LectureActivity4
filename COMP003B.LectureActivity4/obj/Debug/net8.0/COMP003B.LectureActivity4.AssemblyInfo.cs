e91cf3f56ebfe41f02dc643b26707b37ed3c5404
briannaordaz <briannaordaz5@gmail.com>
2025-04-07 22:17:57 -0700

    created a razor view called Index.cshtml

--- COMP003B.LectureActivity4/obj/Debug/net8.0/COMP003B.LectureActivity4.AssemblyInfo.cs
+++ COMP003B.LectureActivity4/obj/Debug/net8.0/COMP003B.LectureActivity4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COMP003B.LectureActivity4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fab0760c862387f38fcc0b1e41804ca61640962")]
 [assembly: System.Reflection.AssemblyProductAttribute("COMP003B.LectureActivity4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COMP003B.LectureActivity4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
